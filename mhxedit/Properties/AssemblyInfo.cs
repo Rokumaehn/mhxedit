0b2d57480151f3dc156763f9951367e2d736287d
Rokumaehn <cdix512@googlemail.com>
2016-02-22 19:15:39 +0100

    corrected Assembly Information

--- mhxedit/Properties/AssemblyInfo.cs
+++ mhxedit/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("mhxedit")]
 [assembly: AssemblyDescription("Savegame Editor for Monster Hunter X\r\nFeatures:\r\n - Edit some Character Values\r\n   (Name, Zenny, Play Time, HR)\r\n - Edit Item Box Item Types and Counts")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("fridget")]
+[assembly: AssemblyCompany("Rokumaehn")]
 [assembly: AssemblyProduct("mhxedit")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
